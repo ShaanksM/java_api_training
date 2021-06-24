af5e880d94f04a92db9aa518ddc72ea139ff496c
ShaanksM <boukritmohamad@gmail.com>
2021-06-24 20:37:02 +0200

    Partie 2 & 3 [ in progress ]

--- src/main/java/fr/lernejo/navy_battle/Launcher.java
+++ src/main/java/fr/lernejo/navy_battle/Launcher.java
@@ -1,6 +1,7 @@
 package fr.lernejo.navy_battle;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
+import fr.lernejo.navy_battle.HeartProject.Requete;
 import fr.lernejo.navy_battle.Implement.ContentSrv;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -9,33 +10,40 @@ import java.util.UUID;
 import java.util.concurrent.Executors;
 
 public class Launcher {
-    private ContentSrv localServer;
-    private ContentSrv remoteServer;
+    private ContentSrv ls;
+    private ContentSrv rs;
 
     public static void main(String[] args) {
         try {
-            if (args.length != 1) {
-                System.err.println("Usage: Launcher [port]");
+            if (args.length == 0) {
+                System.err.println("Usage: Launcher [port] {server_url}");
                 System.exit(-1);
             }
 
-            int port = Integer.parseInt(args[0]);
-            System.out.println("Starting to listen on http://0.0.0.0:" + port + "/");
+            int serverPort = Integer.parseInt(args[0]);
+            System.out.println("Starting to listen on port " + serverPort);
 
-            startServer(port);
+            new Launcher().Sserver(serverPort, args.length > 1 ? args[1] : null);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static void startServer(int port) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-        server.createContext("/ping", Launcher::handlePing);
-        server.start();
+    private void Sserver(int port, String connectURL) throws IOException {
+        ls = new ContentSrv(
+            UUID.randomUUID().toString(),
+            "Hehe pret a affronter Mohamad, jeune Padawan ?",
+            "http://localhost:" + port
+        );
+
+        HttpServer sv = HttpServer.create(new InetSocketAddress(port), 0);
+        sv.createContext("/ping", this::handlePing);
+        sv.setExecutor(Executors.newSingleThreadExecutor());
+        sv.start();
     }
 
-    private static void handlePing(HttpExchange exchange) throws IOException {
-        String body = "Hello";
+    private void handlePing(HttpExchange exchange) throws IOException {
+        String body = "Hellooo :D";
         exchange.sendResponseHeaders(200, body.length());
         try (OutputStream os = exchange.getResponseBody()) { // (1)
             os.write(body.getBytes());
