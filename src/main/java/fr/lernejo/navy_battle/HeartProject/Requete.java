876a8855d978a9c43923f9ac247940153c742533
ShaanksM <boukritmohamad@gmail.com>
2021-06-27 16:26:21 +0200

    Request Server

--- src/main/java/fr/lernejo/navy_battle/HeartProject/Requete.java
+++ src/main/java/fr/lernejo/navy_battle/HeartProject/Requete.java
@@ -10,35 +10,34 @@ import org.json.JSONObject;
 public class Requete {
     private final HttpExchange echng;
 
-    public Requete(HttpExchange exchange) {
-        this.echng = exchange;
-    }
-
-    public String readToString() throws IOException {
-        return new String(this.echng.getRequestBody().readAllBytes());
-    }
-
-    public JSONObject getJSONObject() throws IOException {
-        try {
-            return new JSONObject(readToString());
-        } catch (JSONException e) {
-            sendString(400, e.toString());
-            throw new JSONException(e);
-        }
-    }
-
-    public void sendString(int status, String test) throws IOException{
+    public void sndstrng(int status, String test) throws IOException{
         byte[] bytes = test.getBytes();
         echng.sendResponseHeaders(status, bytes.length);
 
-        try (OutputStream os = echng.getResponseBody()) { // (1)
-            os.write(bytes);
+        try (OutputStream os = echng.getResponseBody()) { os.write(bytes);
         }
         echng.close();
     }
 
+    public Requete(HttpExchange exchange) {
+        this.echng = exchange;
+    }
+
+    public String rdtostrg() throws IOException {
+        return new String( this.echng.getRequestBody().readAllBytes());
+    }
+
+    public JSONObject getJSONObject() throws IOException {
+        try { return new JSONObject(rdtostrg()); }
+
+        catch (JSONException f) {
+            sndstrng(400, f.toString());
+            throw new JSONException(f);
+        }
+    }
+
     public void sendJSON(int status, JSONObject object) throws IOException {
-        echng.getResponseHeaders().set("Content-type", "application/json");
-        sendString(status, object.toString());
+        echng.getResponseHeaders().set("Contenu", "application - json");
+        sndstrng(status, object.toString());
     }
 }
